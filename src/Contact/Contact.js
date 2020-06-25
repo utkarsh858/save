0b31730ed2d65eb6d41ac9d4ba1fb856b34d9eba
Utkarsh Shukla <utkarsh858iitr@gmail.com>
2020-06-25 10:47:57 +0530

    Partially styled contact section
    No gap among input fields
    icons in avataars not coorectly aligned

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,40 +1,62 @@
 import React,{Component} from 'react';
-import {Box,Grid,Typography,Button,TextField,Avatar} from '@material-ui/core';
-import filler from './filler.jpg';
+import {Box,Grid,Typography,Button,TextField,Avatar,
+Link
+} from '@material-ui/core';
+import filler from './filler.png';
+
+
+import FacebookIcon from '@material-ui/icons/Facebook'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import HomeIcon from '@material-ui/icons/Home'
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail'
+import CallIcon from '@material-ui/icons/Call'
 import './Contact.css';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import InstagramIcon from '@material-ui/icons/Instagram';
 
 export default class Contact extends Component{
 
 	render(){
 		return(
 			<Box id="contact">
-			<Typography>
-			Contact Us
+			<Typography gutterBottom variant="h5" display="block" class="section">
+			CONTACT US
 			</Typography>
 			<Grid container spacing={9}>
 			<Grid item xs={12} sm={6}>
-				<form>
-				<TextField id="name" label="Name" variant="filled"/>
+			<Typography gutterBottom variant="body1" class="impression">
+			We'd love to hear from You.
+			</Typography>
+				<form class="form">
+				<TextField id="name" label="Name" variant="outlined" fullWidth/>
 				<br/>
-				<TextField id="email" label="E-mail" variant="filled"/>
+				<TextField id="email" label="E-mail" variant="outlined" fullWidth/>
 				<br/>
-				<TextField id="query" label="Query" variant="filled"/>
+				<TextField id="query" label="Query" variant="outlined" fullWidth multiline/>
 				<Button />
 				</form>
 			</Grid>
 			<Grid item xs={12} sm={6}>
 			<img className="contact" src ={filler}/>
 			<Typography>
-			Phone, Email, etc. 
+			<div class="contact">
+			<CallIcon/> +91-9634689949
+			</div>
+			<div class="contact">
+			<AlternateEmailIcon/> utkarshshukla.gs@gmail.com
+			</div>
+			<div class="contact"> 
+			<HomeIcon/> Bahadurgarh, Haryana - 124507
+			</div>
 			</Typography>
+
 			</Grid>
 			</Grid>
-			<Avatar><FacebookIcon /></Avatar>
-			<Avatar><InstagramIcon /></Avatar>
-			<Avatar><TwitterIcon /></Avatar>
+			<Typography variant="body2" gutterBottom class="center">See us on</Typography>
+			<Grid container spacing={1}>
+			<Avatar class="social" variant="rounded" ><Link><FacebookIcon fontSize="large"/></Link></Avatar>
+			<Avatar class="social" variant="rounded" ><Link><InstagramIcon fontSize="large"/></Link></Avatar>
+			<Avatar class="social" variant="rounded" ><Link><TwitterIcon fontSize="large"/></Link></Avatar>
+			</Grid>
 			</Box>
 		);
 	}
